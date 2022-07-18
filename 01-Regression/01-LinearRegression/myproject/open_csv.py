d2a6ab6da5d79b9f40337876316f924e0431e397
Naveen Crasta <naveen.crasta@gmail.com>
2022-07-18 20:14:10 +0200

    Fix all the flake issues

--- 01-Regression/01-LinearRegression/myproject/open_csv.py
+++ 01-Regression/01-LinearRegression/myproject/open_csv.py
@@ -1,11 +1,8 @@
 import os
-import numpy as np
-import matplotlib.pyplot as plt
-import pandas as pd
-import csv
 
-def ospath(path,n=1):
+
+def ospath(path, n=1):
     for i in range(n):
-        path=os.path.split(path)[0]
-    path=os.path.join(path,'')
+        path = os.path.split(path)[0]
+    path = os.path.join(path, '')
     return path
