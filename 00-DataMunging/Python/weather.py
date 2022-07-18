39aa383ef197ec9c547f91a085f7dd82a1d87c61
Naveen Crasta <naveen.crasta@gmail.com>
2022-07-18 20:10:00 +0200

    Fix flake errors

--- 00-DataMunging/Python/weather.py
+++ 00-DataMunging/Python/weather.py
@@ -1,7 +1,6 @@
 import pandas as pd
 import numpy as np
 from sklearn.neighbors import KNeighborsClassifier
-from sklearn import metrics
 from sklearn.model_selection import train_test_split
 import matplotlib.pyplot as plt
 import argparse
@@ -25,7 +24,7 @@ import argparse
 # def visualize_krange():
 #     k_range = range(1,20)
 #     scores = []
-    
+
 #     for k in k_range:
 #         knn = KNeighborsClassifier(n_neighbors = k)
 #         X_train, X_test, y_train, y_test = train_test_split(X, y)
@@ -58,13 +57,14 @@ import argparse
 
 # visualize_classifier(X, y, t, rfig, knn)
 
+
 class Weather():
     def __init__(self, path: str):
         self._path = path
         self._dataset = pd.read_csv(self._path, parse_dates=True, index_col=0)
         self._header = ['Humidity3pm', 'Pressure3pm', 'Cloud3pm', 'RainTomorrow']
-        self._rfig = plt.figure(figsize=(4,4),dpi=160)
-        self._knn = KNeighborsClassifier(n_neighbors = 5)
+        self._rfig = plt.figure(figsize=(4, 4), dpi=160)
+        self._knn = KNeighborsClassifier(n_neighbors=5)
         self._t = [0.8, 0.7, 0.6, 0.5, 0.4, 0.3, 0.2]
         self._epoch = 100
 
@@ -76,10 +76,10 @@ class Weather():
         self.y = np.array([0 if value == 'No' else 1 for value in self.y])
 
     def visualize_krange(self):
-        k_range = range(1,20)
+        k_range = range(1, 20)
         scores = []
         for k in k_range:
-            knn = KNeighborsClassifier(n_neighbors = k)
+            knn = KNeighborsClassifier(n_neighbors=k)
             X_train, X_test, y_train, y_test = train_test_split(self.X, self.y)
             knn.fit(X_train, y_train)
             scores.append(knn.score(X_test, y_test))
@@ -96,7 +96,9 @@ class Weather():
             scores = []
             for i in range(1, self._epoch):
                 print(i)
-                X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size = 1-s)
+                X_train, X_test, y_train, y_test = train_test_split(self.X,
+                                                                    self.y,
+                                                                    test_size=1-s)
                 self._knn.fit(X_train, y_train)
                 scores.append(self._knn.score(X_test, y_test))
 
@@ -108,14 +110,18 @@ class Weather():
         plt.pause(3)
         plt.close()
 
+
 def main(args):
     weather = Weather(args.path)
     weather.dataprep()
     weather.visualize_krange()
     weather.visualize_classifier()
 
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument('--path', action='store_true', default='../../0-DATA/weather.csv', help='Path. Default: %(default)s')
+    parser.add_argument('--path', action='store_true',
+                        default='../../0-DATA/weather.csv',
+                        help='Path. Default: %(default)s')
     args = parser.parse_args()
     main(args)
