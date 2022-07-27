0b29b726097dacaa08822c1f19681618fe668407
nzeal <nitshukla@yahoo.com>
2022-07-27 10:26:18 +0200

    LinearRegre

---
+++ 02-LinearRegression/linearRegression.py
@@ -0,0 +1,40 @@
+import numpy as np
+
+class LinearRegression:
+    """ Linear Regression Using Gradient Descent
+    Initilization: 
+        lr = learning rate 
+        niters = number of iterations
+        weights = none
+        bais =  none
+    """
+
+    def __init__(self, learning_rate = 0.01, niters=1000):
+        self.lr = learning_rate
+        self.niters = niters
+        self.weights = None 
+        self.bias = None
+
+    def fit(self, X, y):
+        """ Fitting the traininng data
+        x: n_features
+        y: n_target_value
+        """
+        n_samples, n_features = X.shape
+        self.weights = np.zeros(n_features)
+        self.bias    = 0
+
+        for _ in range(self.niters):
+            y_pred = np.dot(X,self.weights) + self.bias
+            residuals = y_pred - y 
+            # Compute gradient 
+            dw = (1/n_samples) * np.dot(X.T, residuals)
+            db = (1/n_samples) * np.sum(residuals)
+
+            #update paraemeters 
+            self.weights -= self.lr * dw
+            self.bias    -= self.lr * db
+
+    def predict(self, X):
+        y_approx = np.dot(X, self.weights) + self.bias
+        return y_approx
