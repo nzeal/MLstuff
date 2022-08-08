48d57208308e1f598931c019ee78881a57f7b6a5
Nitin Shukla <n.shukla@cineca.it>
2022-08-08 12:01:21 +0200

    correction data process julia

--- DSMLwithJulia/Dataprocessing/dataprocesswithScilearn.jl
+++ DSMLwithJulia/Dataprocessing/dataprocesswithScilearn.jl
@@ -27,3 +27,19 @@ mapper = DataFrameMapper([
     (:Purchased, LabelBinarizer())])
 
 Y = fit_transform!(mapper2, copy(dataset))
+
+## Splitting the dataset into the Training set and Test set
+using ScikitLearn.CrossValidation: train_test_split
+
+X_train, X_test, y_train, y_test = train_test_split(X, Y, test_size=0.2, random_state=1)
+
+## Feature Scaling
+
+@sk_import preprocessing: (LabelBinarizer, StandardScaler)
+
+sc = StandardScaler()
+X_train[:, 4:5] = sc.fit_transform(X_train[:, 4:5]);
+X_test[:, 4:5] = sc.transform(X_test[:, 4:5]);
+
+print(X_train, X_test)
+
